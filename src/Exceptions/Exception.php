d132e7f275a54013a32d09f094e0a4eda59be1ac
her-cat <i@her-cat.com>
2019-07-16 13:51:40 +0000

    Apply fixes from StyleCI

--- src/Exceptions/Exception.php
+++ src/Exceptions/Exception.php
@@ -1,8 +1,16 @@
 <?php
 
+/*
+ * This file is part of the her-cat/baidu-translator.
+ *
+ * (c) her-cat <i@her-cat.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace HerCat\BaiduTranslator\Exceptions;
 
 class Exception extends \Exception
 {
-
 }
